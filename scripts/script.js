0180c0d15c3206c33187a3187957a1a68da9db66
the-log <2018ankurm@gmail.com>
2017-03-26 07:21:35 -0400

    test

--- scripts/script.js
+++ scripts/script.js
@@ -108,47 +108,4 @@ function sendRequest(){
     // its going to have a bunch of jargon so what you are going to have to do is remove any text uptil the text state's its DOI
 });
 }
-function abstractGrabber(data){
-  console.log(data);
-  array = data["search-results"].entry;
-  console.log(array);
-  // for (var i = 0; i < array.length; i++) {
-  //   var entry = array[i];
-  //   var doi = "doi/" + entry["prism:doi"];
-  //     var title = entry["dc:title"];
-  //     //console.log(title);
-  //     if (typeof title === 'undefined' || title === null) {
-  //       console.log("Error");
-  //       return;
-  //     }
-  //     var res = title.split(" ");
-  //     for (var i = 0; i < res.length; i++) {
-  //       res[i] = res[i].toLowerCase();
-  //       res[i].replace(/['“]+/g, '');
-  //       if (commonWords.has(res[i].toLowerCase()) == false && res[i] != "" && res[i].length > 3){
-  //         if(myMap.has(res[i])){
-  //           var doiList = myMap.get(res[i]);
-  //           doiList.push(doi);
-  //           myMap.set(res[i], doiList)
-  //         }
-  //         else{
-  //           var doiList = [];
-  //           doiList.push(doi);
-  //           myMap.set(res[i], doiList)
-  //         }
-  //       }
-  //       if(myMap.size == 1908){
-  //         console.log("Done");
-  //         a = [];
-  //         for(var x of myMap)
-  //           a.push(x);
-  //         a.sort(function(x, y) {
-  //           return y[1].length - x[1].length;
-  //         });
-  //         myMap = new Map(a);
-  //         console.log(myMap);
-  //         break;
-  //       }
-  //     }
-  // }
 }
