1e180c24d1e1b81b7d43a447160c0246ab0e148a
the-log <2018ankurm@gmail.com>
2017-03-26 06:31:54 -0400

    Working on displaying links

--- scripts/script.js
+++ scripts/script.js
@@ -4,17 +4,25 @@ var gayshit;
 var freqs = []
 var b = false;
 var l = ["Journal of American College of Surgeons", "Surgery", "International Journal of Surgery", "British Journal of Oral and Maxillofacial Surgery", "The Journal of Heart and Lung Transplantation", "Annals of Vascular Surgery"];
-var commonWords = new Set(['a', 'about', 'above', 'after', 'again', 'against', 'all', 'am', 'an', 'and', 'any', 'are', "aren't", 'as', 'at', 'be', 'because', 'been', 'before', 'being', 'below', 'between', 'both', 'but', 'by', "can't", 'cannot', 'could', "couldn't", 'did', "didn't", 'do', 'does', "doesn't", 'doing', "don't", 'down', 'during', 'each', 'few', 'for', 'from', 'further', 'had', "hadn't", 'has', "hasn't", 'have', "haven't", 'having', 'he', "he'd", "he'll", "he's", 'her', 'here', "here's", 'hers', 'herself', 'him', 'himself', 'his', 'how', "how's", 'i', "i'd", "i'll", "i'm", "i've", 'if', 'in', 'into', 'is', "isn't", 'it', "it's", 'its', 'itself', "let's", 'me', 'more', 'most', "mustn't", 'my', 'myself', 'no', 'nor', 'not', 'of', 'off', 'on', 'once', 'only', 'or', 'other', 'ought', 'our', 'ours\tourselves', 'out', 'over', 'own', 'same', "shan't", 'she', "she'd", "she'll", "she's", 'should', "shouldn't", 'so', 'some', 'such', 'than', 'that', "that's", 'the', 'their', 'theirs', 'them', 'themselves', 'then', 'there', "there's", 'these', 'they', "they'd", "they'll", "they're", "they've", 'this', 'those', 'through', 'to', 'too', 'under', 'until', 'up', 'very', 'was', "wasn't", 'we', "we'd", "we'll", "we're", "we've", 'were', "weren't", 'what', "what's", 'when', "when's", 'where', "where's", 'which', 'while', 'who', "who's", 'whom', 'why', "why's", 'with', "won't", 'would', "wouldn't", 'you', "you'd", "you'll", "you're", "you've", 'your', 'yours', 'yourself', 'yourselves', 'review', "surgery", "report", "peer", "patients", "systematic", "surgical", "management", "meta-analysis", "versus", "total", "factors", "controlled", "repair", "cancer", "randomized", "study", "outcomes", "reconstruction", "safety", "open", "analysis", 'meta-analysis”', '“comparison', '“the', 'study”', "cancer:"]);
+var commonWords = new Set(['a', 'about', 'above', 'after', 'again', 'against', 'all', 'am', 'an', 'and', 'any', 'are', "aren't", 'as', 'at', 'be', 'because', 'been', 'before', 'being', 'below', 'between', 'both', 'but', 'by', "can't", 'cannot', 'could', "couldn't", 'did', "didn't", 'do', 'does', "doesn't", 'doing', "don't", 'down', 'during', 'each', 'few', 'for', 'from', 'further', 'had', "hadn't", 'has', "hasn't", 'have', "haven't", 'having', 'he', "he'd", "he'll", "he's", 'her', 'here', "here's", 'hers', 'herself', 'him', 'himself', 'his', 'how', "how's", 'i', "i'd", "i'll", "i'm", "i've", 'if', 'in', 'into', 'is', "isn't", 'it', "it's", 'its', 'itself', "let's", 'me', 'more', 'most', "mustn't", 'my', 'myself', 'no', 'nor', 'not', 'of', 'off', 'on', 'once', 'only', 'or', 'other', 'ought', 'our', 'ours\tourselves', 'out', 'over', 'own', 'same', "shan't", 'she', "she'd", "she'll", "she's", 'should', "shouldn't", 'so', 'some', 'such', 'than', 'that', "that's", 'the', 'their', 'theirs', 'them', 'themselves', 'then', 'there', "there's", 'these', 'they', "they'd", "they'll", "they're", "they've", 'this', 'those', 'through', 'to', 'too', 'under', 'until', 'up', 'very', 'was', "wasn't", 'we', "we'd", "we'll", "we're", "we've", 'were', "weren't", 'what', "what's", 'when', "when's", 'where', "where's", 'which', 'while', 'who', "who's", 'whom', 'why', "why's", 'with', "won't", 'would', "wouldn't", 'you', "you'd", "you'll", "you're", "you've", 'your', 'yours', 'yourself', 'yourselves', 'review', "surgery", "report", "peer", "patients", "systematic", "surgical", "management", "meta-analysis", "versus", "total", "factors", "controlled", "repair", "cancer", "randomized", "study", "outcomes", "reconstruction", "safety", "open", "analysis", 'meta-analysis”', '“comparison', '“the', 'study”', "cancer:", "clinical"]);
 
 var myMap = new Map();
-//
-// for (i of stringMap)
-// {
-//   myMap.set(i[0], new Set(i[1]));
-// }
-//
+
+for (i of stringMap)
+{
+  myMap.set(i[0], new Set(i[1]));
+}
+
 console.log(commonWords);
-//
+
+console.log("Done");
+a = [];
+for(var x of myMap)
+  a.push(x);
+a.sort(function(x, y) {
+  return y[1].size - x[1].size;
+});
+myMap = new Map(a);
 console.log(myMap);
 
 
@@ -23,7 +31,7 @@ console.log(myMap);
 /*if(d.getDay == 0){ //Calls this weekly
   sendRequest();
 }*/
-sendRequest();
+// sendRequest();
 
 function sendRequest(){
     for (var i = 0; i < l.length; i++) {
@@ -47,7 +55,7 @@ function sendRequest(){
 			  data: { url : jsonURL }
 			})
 		  .done(function( json_contents ) {
-        console.log(json_contents);
+        //console.log(json_contents);
         abstractGrabber(json_contents)
 		  });
     }
@@ -60,11 +68,23 @@ function sendRequest(){
 }
 function abstractGrabber(data){
   array = data["search-results"].entry;
+  var aSearchURL = "http://api.elsevier.com/content/article/"
+  var format = "?httpAccept=application/json";
   for (var i = 0; i < array.length; i++) {
     var entry = array[i];
     var doi = "doi/" + entry["prism:doi"];
+    //console.log(aSearchURL+doi+format);
+    abstractURL = aSearchURL + doi + format;
+    $.ajax({
+      method: "GET",
+      url: "worker.php",
+      dataType: "json",
+      data: { url : abstractURL }
+    })
+    .done(function( json_contents ) {
       counter = counter + 1;
-      var title = entry["dc:title"];
+      gayshit = json_contents;
+      var title = json_contents["full-text-retrieval-response"]["coredata"]["dc:title"];
       if (typeof title === 'undefined' || title === null) {
         console.log("Error");
         return;
@@ -85,18 +105,19 @@ function abstractGrabber(data){
             myMap.set(res[i], doiList)
           }
         }
-        // if(myMap.size == 1908){
-        //   console.log("Done");
-        //   a = [];
-        //   for(var x of myMap)
-        //     a.push(x);
-        //   a.sort(function(x, y) {
-        //     return y[1].length - x[1].length;
-        //   });
-        //   myMap = new Map(a);
-        //   console.log(myMap);
-        //   break;
-        // }
+        if(myMap.size == 1908){
+          console.log("Done");
+          a = [];
+          for(var x of myMap)
+            a.push(x);
+          a.sort(function(x, y) {
+            return y[1].length - x[1].length;
+          });
+          myMap = new Map(a);
+          console.log(myMap);
+          break;
+        }
       }
+    });
   }
 }
