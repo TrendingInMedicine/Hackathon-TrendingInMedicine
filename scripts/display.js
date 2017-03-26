87f8ded94c48f90a8483fbd3b1e4b1707729d47c
the-log <2018ankurm@gmail.com>
2017-03-26 01:09:46 -0400

    Working on displaying

--- scripts/display.js
+++ scripts/display.js
@@ -2,6 +2,11 @@ function display(){
 	 $('body').on('click', 'a', function() {
     	var id = $(this).attr('id');
     	console.log(id);
+			if(id = "mar2017"){
+				for (var i = 0; i < 10; i++) {
+					console.log(myMap[i]);
+					document.getElementById("output").innerHTML = myMap[i];
+				}
+			}
 		});
-		console.log(myMap);
 }
