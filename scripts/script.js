899a97889524e51e90849f1ed604b7ebe6583fd3
the-log <2018ankurm@gmail.com>
2017-03-26 06:19:53 -0400

    Working on displaying links

--- scripts/script.js
+++ scripts/script.js
@@ -7,14 +7,14 @@ var l = ["Journal of American College of Surgeons", "Surgery", "International Jo
 var commonWords = new Set(['a', 'about', 'above', 'after', 'again', 'against', 'all', 'am', 'an', 'and', 'any', 'are', "aren't", 'as', 'at', 'be', 'because', 'been', 'before', 'being', 'below', 'between', 'both', 'but', 'by', "can't", 'cannot', 'could', "couldn't", 'did', "didn't", 'do', 'does', "doesn't", 'doing', "don't", 'down', 'during', 'each', 'few', 'for', 'from', 'further', 'had', "hadn't", 'has', "hasn't", 'have', "haven't", 'having', 'he', "he'd", "he'll", "he's", 'her', 'here', "here's", 'hers', 'herself', 'him', 'himself', 'his', 'how', "how's", 'i', "i'd", "i'll", "i'm", "i've", 'if', 'in', 'into', 'is', "isn't", 'it', "it's", 'its', 'itself', "let's", 'me', 'more', 'most', "mustn't", 'my', 'myself', 'no', 'nor', 'not', 'of', 'off', 'on', 'once', 'only', 'or', 'other', 'ought', 'our', 'ours\tourselves', 'out', 'over', 'own', 'same', "shan't", 'she', "she'd", "she'll", "she's", 'should', "shouldn't", 'so', 'some', 'such', 'than', 'that', "that's", 'the', 'their', 'theirs', 'them', 'themselves', 'then', 'there', "there's", 'these', 'they', "they'd", "they'll", "they're", "they've", 'this', 'those', 'through', 'to', 'too', 'under', 'until', 'up', 'very', 'was', "wasn't", 'we', "we'd", "we'll", "we're", "we've", 'were', "weren't", 'what', "what's", 'when', "when's", 'where', "where's", 'which', 'while', 'who', "who's", 'whom', 'why', "why's", 'with', "won't", 'would', "wouldn't", 'you', "you'd", "you'll", "you're", "you've", 'your', 'yours', 'yourself', 'yourselves', 'review', "surgery", "report", "peer", "patients", "systematic", "surgical", "management", "meta-analysis", "versus", "total", "factors", "controlled", "repair", "cancer", "randomized", "study", "outcomes", "reconstruction", "safety", "open", "analysis", 'meta-analysis”', '“comparison', '“the', 'study”', "cancer:"]);
 
 var myMap = new Map();
-
-for (i of stringMap)
-{
-  myMap.set(i[0], new Set(i[1]));
-}
-
+//
+// for (i of stringMap)
+// {
+//   myMap.set(i[0], new Set(i[1]));
+// }
+//
 console.log(commonWords);
-
+//
 console.log(myMap);
 
 
@@ -23,7 +23,7 @@ console.log(myMap);
 /*if(d.getDay == 0){ //Calls this weekly
   sendRequest();
 }*/
-// sendRequest();
+sendRequest();
 
 function sendRequest(){
     for (var i = 0; i < l.length; i++) {
@@ -47,8 +47,8 @@ function sendRequest(){
 			  data: { url : jsonURL }
 			})
 		  .done(function( json_contents ) {
-        //console.log(json_contents);
-        abstractGrabber(json_contents)
+        console.log(json_contents);
+        //abstractGrabber(json_contents)
 		  });
     }
 
@@ -65,18 +65,8 @@ function abstractGrabber(data){
   for (var i = 0; i < array.length; i++) {
     var entry = array[i];
     var doi = "doi/" + entry["prism:doi"];
-    //console.log(aSearchURL+doi+format);
-    abstractURL = aSearchURL + doi + format;
-    $.ajax({
-      method: "GET",
-      url: "worker.php",
-      dataType: "json",
-      data: { url : abstractURL }
-    })
-    .done(function( json_contents ) {
       counter = counter + 1;
-      gayshit = json_contents;
-      var title = json_contents["full-text-retrieval-response"]["coredata"]["dc:title"];
+      var title = data["full-text-retrieval-response"]["coredata"]["dc:title"];
       if (typeof title === 'undefined' || title === null) {
         console.log("Error");
         return;
