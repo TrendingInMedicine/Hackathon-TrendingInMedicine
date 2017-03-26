02a59f933c3fd3cc4cf168cb374a5c49adf81a23
the-log <2018ankurm@gmail.com>
2017-03-25 20:42:18 -0400

    Looking for undefined title

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 console.log("Loaded");
 var counter = 0;
 var gayshit;
-var l = ["Journal of American College of Surgeons", "Surgery", "International Journal of Surgery", "The Surgeon", "British Journal of Oral and Maxillofacial Surgery", "The Journal of Heart and Lung Transplantation", "Annals of Vascular Surgery"];
+var l = ["Journal of American College of Surgeons", "Surgery", "International Journal of Surgery", "British Journal of Oral and Maxillofacial Surgery", "The Journal of Heart and Lung Transplantation", "Annals of Vascular Surgery"];
 var commonWords = new Set(['a', 'about', 'above', 'after', 'again', 'against', 'all', 'am', 'an', 'and', 'any', 'are', "aren't", 'as', 'at', 'be', 'because', 'been', 'before', 'being', 'below', 'between', 'both', 'but', 'by', "can't", 'cannot', 'could', "couldn't", 'did', "didn't", 'do', 'does', "doesn't", 'doing', "don't", 'down', 'during', 'each', 'few', 'for', 'from', 'further', 'had', "hadn't", 'has', "hasn't", 'have', "haven't", 'having', 'he', "he'd", "he'll", "he's", 'her', 'here', "here's", 'hers', 'herself', 'him', 'himself', 'his', 'how', "how's", 'i', "i'd", "i'll", "i'm", "i've", 'if', 'in', 'into', 'is', "isn't", 'it', "it's", 'its', 'itself', "let's", 'me', 'more', 'most', "mustn't", 'my', 'myself', 'no', 'nor', 'not', 'of', 'off', 'on', 'once', 'only', 'or', 'other', 'ought', 'our', 'ours\tourselves', 'out', 'over', 'own', 'same', "shan't", 'she', "she'd", "she'll", "she's", 'should', "shouldn't", 'so', 'some', 'such', 'than', 'that', "that's", 'the', 'their', 'theirs', 'them', 'themselves', 'then', 'there', "there's", 'these', 'they', "they'd", "they'll", "they're", "they've", 'this', 'those', 'through', 'to', 'too', 'under', 'until', 'up', 'very', 'was', "wasn't", 'we', "we'd", "we'll", "we're", "we've", 'were', "weren't", 'what', "what's", 'when', "when's", 'where', "where's", 'which', 'while', 'who', "who's", 'whom', 'why', "why's", 'with', "won't", 'would', "wouldn't", 'you', "you'd", "you'll", "you're", "you've", 'your', 'yours', 'yourself', 'yourselves', 'review', "surgery"]);
 var myMap = new Map()
 
@@ -76,6 +76,10 @@ function abstractGrabber(data){
       }
       gayshit = json_contents;
       var title = json_contents["full-text-retrieval-response"]["coredata"]["dc:title"];
+      if (typeof title === 'undefined' || title === null) {
+        console.log("Im stupid");
+        return;
+      }
       var res = title.split(" ");
       for (var i = 0; i < res.length; i++) {
         res[i] = res[i].toLowerCase();
