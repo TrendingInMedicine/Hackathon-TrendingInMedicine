24c2ee835fd24f2a779f982c616051293c419152
the-log <2018ankurm@gmail.com>
2017-03-26 10:16:33 -0400

    Displaying Debug

--- scripts/display.js
+++ scripts/display.js
@@ -1,4 +1,4 @@
-var li = [];
+var li = myLi;
 var lengths = [];
 count = 0
 var j = 1;
@@ -10,75 +10,97 @@ function display(){
 		if((id != "uneeded" || typeof id != 'undefined' || id != null) && (count === 0)){
 			count = count + 1;
 			document.getElementById("output").innerHTML = "";
-			var i= 0;
+			var i = 0;
 			if(id == "mar2017"){
-				for(var x of myMap)  {
-					var temp_json = "";
-					j = 1;
-					i = i + 1;
-					//console.log(i);
-					if(i > 10)
+				for (var m of myMap.entries())
+				{
+					if(i>10)
 					break;
-					//	console.log(x);
-					var myDiv = document.createElement("DIV");
-					myDiv.setAttribute("id", "top" + i);
-					myDiv.innerHTML = "#" + x[0];
-					$('#output').append(myDiv);
-					var aSearchURL = "http://api.elsevier.com/content/article/"
-					var format = "?httpAccept=application/json";
-					//var j = 0;
-					for (var y of x[1]) {
-						//console.log(x[1].size);
-						lengths.push(x[1].size);
-						var doi = y;
-						var abstractURL = aSearchURL + doi + format;
-						//console.log(abstractURL);
-						$.ajax({
-							method: "GET",
-							url: "worker.php",
-							dataType: "json",
-							data: { url : abstractURL }
-						})
-						.done(function( json_contents ) {
-							temp_json = json_contents;
-							console.log(temp_json);
-							// 					//console.log(i);
-							var data = temp_json["full-text-retrieval-response"]["coredata"];
-							console.log(data);
-							var title = data["dc:title"]
-							var url = data["link"][1]["@href"]
-							var doi = data["prism:doi"]
-							var pn = data["prism:publicationName"]
-							var date = data["prism:coverDate"]
-							var desc = title + ". " + pn + " " + date + " " + doi;
-							console.log(desc);
-							li.push(desc);
-							// 					// li.push(desc);
-							// 					// if(li.length == 52){
-							// 					// 	addList();
-							// 					// }
-							var desDiv = document.createElement("a");
-							desDiv.setAttribute("class", "snow")
-							var br = document.createElement("br");
-							desDiv.innerHTML = desc;
-							desDiv.href = url;
-							var top = "#top" + j;
-							console.log(top);
-							$(top).append(br);
-							$(top).append(desDiv);
-							//
-							// 					$("#result" + i).html(x[0]);
-							// 			  });
-							// 			}
-							// 			myDiv.setAttribute("class", "boshal");
-							//
-							// 		}
-							// 	}
-							// });
-						});
-					}
-
-				//	j = j + 1;
+					for(var index = 0; index < m[1].size; index++)
+					{
+				var myDiv = document.createElement("DIV");
+				myDiv.setAttribute("id", "top" + i);
+	    	myDiv.innerHTML = "#" + m[0];
+			  $('#output').append(myDiv);
+				var desDiv = document.createElement("p");
+				desDiv.setAttribute("class", "snow")
+				var br = document.createElement("br");
+				desDiv.innerHTML = myLi[index];
+				//desDiv.href = url;
+				var top = "#top" + i;
+				console.log(top);
+				$(top).append(br);
+				$(top).append(desDiv);
+			}
+				i = i + 1;
+			}
+				// for(var x of myMap)  {
+				// 	var temp_json = "";
+				// 	j = 1;
+				// 	i = i + 1;
+				// 	//console.log(i);
+				// 	if(i > 10)
+				// 	break;
+				// 	//	console.log(x);
+				// 	var myDiv = document.createElement("DIV");
+				// 	myDiv.setAttribute("id", "top" + i);
+				// 	myDiv.innerHTML = "#" + x[0];
+				// 	$('#output').append(myDiv);
+				// 	var aSearchURL = "http://api.elsevier.com/content/article/"
+				// 	var format = "?httpAccept=application/json";
+				// 	//var j = 0;
+				// 	for (var y of x[1]) {
+				// 		//console.log(x[1].size);
+				// 		lengths.push(x[1].size);
+				// 		var doi = y;
+				// 		var abstractURL = aSearchURL + doi + format;
+				// 		//console.log(abstractURL);
+				// 		$.ajax({
+				// 			method: "GET",
+				// 			url: "worker.php",
+				// 			dataType: "json",
+				// 			data: { url : abstractURL }
+				// 		})
+				// 		.done(function( json_contents ) {
+				// 			temp_json = json_contents;
+				// 			console.log(temp_json);
+				// 			// 					//console.log(i);
+				// 			var data = temp_json["full-text-retrieval-response"]["coredata"];
+				// 			console.log(data);
+				// 			var title = data["dc:title"]
+				// 			var url = data["link"][1]["@href"]
+				// 			var doi = data["prism:doi"]
+				// 			var pn = data["prism:publicationName"]
+				// 			var date = data["prism:coverDate"]
+				// 			var desc = title + ". " + pn + " " + date + " " + doi;
+				// 			console.log(desc);
+				// 			li.push(desc);
+				// 			// 					// li.push(desc);
+				// 			// 					// if(li.length == 52){
+				// 			// 					// 	addList();
+				// 			// 					// }
+				// 			var desDiv = document.createElement("a");
+				// 			desDiv.setAttribute("class", "snow")
+				// 			var br = document.createElement("br");
+				// 			desDiv.innerHTML = desc;
+				// 			desDiv.href = url;
+				// 			var top = "#top" + j;
+				// 			console.log(top);
+				// 			$(top).append(br);
+				// 			$(top).append(desDiv);
+				// 			//
+				// 			// 					$("#result" + i).html(x[0]);
+				// 			// 			  });
+				// 			// 			}
+				// 			// 			myDiv.setAttribute("class", "boshal");
+				// 			//
+				// 			// 		}
+				// 			// 	}
+				// 			// });
+				// 		});
+				// 	}
+				//
+				// //	j = j + 1;
 
 				}
 			}
