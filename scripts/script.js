4369a257af197e05cd608d705bb03cbc9240dea2
the-log <2018ankurm@gmail.com>
2017-03-26 03:08:30 -0400

    Working on displaying links

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,7 @@ var freqs = []
 var b = false;
 var l = ["Journal of American College of Surgeons", "Surgery", "International Journal of Surgery", "British Journal of Oral and Maxillofacial Surgery", "The Journal of Heart and Lung Transplantation", "Annals of Vascular Surgery"];
 var commonWords = new Set(['a', 'about', 'above', 'after', 'again', 'against', 'all', 'am', 'an', 'and', 'any', 'are', "aren't", 'as', 'at', 'be', 'because', 'been', 'before', 'being', 'below', 'between', 'both', 'but', 'by', "can't", 'cannot', 'could', "couldn't", 'did', "didn't", 'do', 'does', "doesn't", 'doing', "don't", 'down', 'during', 'each', 'few', 'for', 'from', 'further', 'had', "hadn't", 'has', "hasn't", 'have', "haven't", 'having', 'he', "he'd", "he'll", "he's", 'her', 'here', "here's", 'hers', 'herself', 'him', 'himself', 'his', 'how', "how's", 'i', "i'd", "i'll", "i'm", "i've", 'if', 'in', 'into', 'is', "isn't", 'it', "it's", 'its', 'itself', "let's", 'me', 'more', 'most', "mustn't", 'my', 'myself', 'no', 'nor', 'not', 'of', 'off', 'on', 'once', 'only', 'or', 'other', 'ought', 'our', 'ours\tourselves', 'out', 'over', 'own', 'same', "shan't", 'she', "she'd", "she'll", "she's", 'should', "shouldn't", 'so', 'some', 'such', 'than', 'that', "that's", 'the', 'their', 'theirs', 'them', 'themselves', 'then', 'there', "there's", 'these', 'they', "they'd", "they'll", "they're", "they've", 'this', 'those', 'through', 'to', 'too', 'under', 'until', 'up', 'very', 'was', "wasn't", 'we', "we'd", "we'll", "we're", "we've", 'were', "weren't", 'what', "what's", 'when', "when's", 'where', "where's", 'which', 'while', 'who', "who's", 'whom', 'why', "why's", 'with', "won't", 'would', "wouldn't", 'you', "you'd", "you'll", "you're", "you've", 'your', 'yours', 'yourself', 'yourselves', 'review', "surgery", "report", "peer", "patients", "systematic", "surgical", "management", "meta-analysis", "versus", "total", "factors", "controlled", "repair", "cancer", "randomized", "study", "outcomes", "reconstruction", "safety", "open", "analysis", 'meta-analysis”', '“comparison', '“the', 'study”', "cancer:"]);
-var myMap = new Map();
+var myMap = new Map()
 
 console.log(commonWords);
 
@@ -36,7 +36,6 @@ function sendRequest(){
 			  data: { url : jsonURL }
 			})
 		  .done(function( json_contents ) {
-        console.log("Done");
         //console.log(json_contents);
         abstractGrabber(json_contents)
 		  });
@@ -54,12 +53,6 @@ function abstractGrabber(data){
   for (var i = 0; i < array.length; i++) {
     var entry = array[i];
     var doi = "doi/" + entry["prism:doi"];
-    var title = entry["dc:title"]
-    var url = entry["link"][1]
-    var pn = entry["prism:publicationName"]
-    var date = entry["prism:coverDate"]
-    var desc = title + " " + pn + " " + date + " " + doi;
-    console.log(desc);
     //console.log(aSearchURL+doi+format);
     abstractURL = aSearchURL + doi + format;
     $.ajax({
@@ -83,12 +76,12 @@ function abstractGrabber(data){
         if (commonWords.has(res[i].toLowerCase()) == false && res[i] != "" && res[i].length > 3){
           if(myMap.has(res[i])){
             var doiList = myMap.get(res[i]);
-            doiList.add(desc;
+            doiList.add(doi);
             myMap.set(res[i], doiList)
           }
           else{
             var doiList = new Set();
-            doiList.add(desc);
+            doiList.add(doi);
             myMap.set(res[i], doiList)
           }
         }
