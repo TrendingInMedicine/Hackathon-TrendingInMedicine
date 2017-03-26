051dad9f3bf59968c7dc609d800927fc3b480aac
the-log <2018ankurm@gmail.com>
2017-03-25 22:18:32 -0400

    Fixing undefined title

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,7 @@ console.log("Loaded");
 var counter = 0;
 var gayshit;
 var freqs = []
+var b = false;
 var l = ["Journal of American College of Surgeons", "Surgery", "International Journal of Surgery", "British Journal of Oral and Maxillofacial Surgery", "The Journal of Heart and Lung Transplantation", "Annals of Vascular Surgery"];
 var commonWords = new Set(['a', 'about', 'above', 'after', 'again', 'against', 'all', 'am', 'an', 'and', 'any', 'are', "aren't", 'as', 'at', 'be', 'because', 'been', 'before', 'being', 'below', 'between', 'both', 'but', 'by', "can't", 'cannot', 'could', "couldn't", 'did', "didn't", 'do', 'does', "doesn't", 'doing', "don't", 'down', 'during', 'each', 'few', 'for', 'from', 'further', 'had', "hadn't", 'has', "hasn't", 'have', "haven't", 'having', 'he', "he'd", "he'll", "he's", 'her', 'here', "here's", 'hers', 'herself', 'him', 'himself', 'his', 'how', "how's", 'i', "i'd", "i'll", "i'm", "i've", 'if', 'in', 'into', 'is', "isn't", 'it', "it's", 'its', 'itself', "let's", 'me', 'more', 'most', "mustn't", 'my', 'myself', 'no', 'nor', 'not', 'of', 'off', 'on', 'once', 'only', 'or', 'other', 'ought', 'our', 'ours\tourselves', 'out', 'over', 'own', 'same', "shan't", 'she', "she'd", "she'll", "she's", 'should', "shouldn't", 'so', 'some', 'such', 'than', 'that', "that's", 'the', 'their', 'theirs', 'them', 'themselves', 'then', 'there', "there's", 'these', 'they', "they'd", "they'll", "they're", "they've", 'this', 'those', 'through', 'to', 'too', 'under', 'until', 'up', 'very', 'was', "wasn't", 'we', "we'd", "we'll", "we're", "we've", 'were', "weren't", 'what', "what's", 'when', "when's", 'where', "where's", 'which', 'while', 'who', "who's", 'whom', 'why', "why's", 'with', "won't", 'would', "wouldn't", 'you', "you'd", "you'll", "you're", "you've", 'your', 'yours', 'yourself', 'yourselves', 'review', "surgery"]);
 var myMap = new Map()
@@ -13,15 +14,6 @@ var d = new Date();
   sendRequest();
 }*/
 sendRequest();
-console.log("Done");
-a = [];
-for(var x of myMap)
-  a.push(x);
-a.sort(function(x, y) {
-  return x[1].length - y[1].length;
-});
-myMap = new Map(a);
-console.log(myMap);
 
 function sendRequest(){
     for (var i = 0; i < l.length; i++) {
@@ -48,6 +40,17 @@ function sendRequest(){
         //console.log(json_contents);
         abstractGrabber(json_contents)
 		  });
+    }
+    if(b){
+      console.log("Done");
+      a = [];
+      for(var x of myMap)
+        a.push(x);
+      a.sort(function(x, y) {
+        return x[1].length - y[1].length;
+      });
+      myMap = new Map(a);
+      console.log(myMap);
     }
     // grab each articles DOI and parse them
     // http://api.elsevier.com/content/article/[doi]?httpAccept=application/json - format to get abstract and other shit
@@ -97,4 +100,5 @@ function abstractGrabber(data){
       }
     });
   }
+  b = true;
 }
