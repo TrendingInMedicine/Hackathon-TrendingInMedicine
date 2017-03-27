cbe726db730f355c424e9e859a2089a6c30b28b1
Anjam Alam <2018aalam@sonic.csl.tjhsst.edu>
2017-03-27 15:17:17 -0400

    fixing display

--- scripts/display.js
+++ scripts/display.js
@@ -3,6 +3,7 @@ var links = mylinks;
 var lengths = [];
 count = 0
 var j = 1;
+var snowshal = 0;
 function display(){
 	$('body').on('click', 'a', function() {
 
@@ -22,7 +23,7 @@ function display(){
 					myDiv.setAttribute("id", "top" + i);
 					myDiv.innerHTML = "#" + m[0];
 					$('#output').append(myDiv);
-					for(var index = 0; index < m[1].size; index++)
+					for(var index = snowshal; index < m[1].size; index++)
 					{
 
 				var desDiv = document.createElement("a");
